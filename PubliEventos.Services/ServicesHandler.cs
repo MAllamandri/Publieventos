158b87f0f8ff48e42cacbff1987e62c3b650bbeb
MAllamandri <maxi_alias17@hotmail.com>
2014-08-05 21:10:28 -0300

    Servicio de localidades.

---
+++ PubliEventos.Services/ServicesHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace PubliEventos.Services
+{
+    using System.Collections.Generic;
+    using PubliEventos.Contract.Contracts;
+    using PubliEventos.Domain.Domain;
+    using PubliEventos.Services.Services;
+
+    /// <summary>
+    /// Manejador de servicios.
+    /// </summary>
+    public class ServicesHandler : IServiceLocalities
+    {
+        /// <summary>
+        /// Obtiene todas las localidades.
+        /// </summary>
+        /// <returns>Lista de localidades.</returns>
+        public List<Locality> GetAllLocalities()
+        {
+            return ServiceLocalities.GetAllLocalities();
+        }
+    }
+}
