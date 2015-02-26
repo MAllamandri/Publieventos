3ba7c399e7ee0b12198659a77bb93dc94b7e69a9
mallamandri <maxi_alias17@hotmail.com>
2015-02-26 18:35:48 -0300

    Compartir eventos en facebook y twitter

---
+++ PubliEventos.Web/Helpers/SocialLink.cs
@@ -0,0 +1,35 @@
+﻿namespace PubliEventos.Web.Helpers
+{
+    using System.Web.Mvc;
+
+    public static class SocialLink
+    {
+        /// <summary>
+        /// Comparte un evento en twitter.
+        /// </summary>
+        /// <param name="htmlHelper">Helper.</param>
+        /// <param name="title">Titulo del evento.</param>
+        /// <param name="eventId">Identificador del evento.</param>
+        /// <param name="imageUrl">Path de la imagen.</param>
+        /// <returns></returns>
+        public static MvcHtmlString SharedTwitter(this HtmlHelper htmlHelper, string title, string eventId, string imageUrl)
+        {
+            //declare the html helper 
+            var builder = new TagBuilder("a");
+            var url = string.Format("{0}://{1}/Event/Detail/{2}", System.Web.HttpContext.Current.Request.Url.Scheme, System.Web.HttpContext.Current.Request.Url.Authority, eventId);
+            var imgBuilder = new TagBuilder("img");
+
+            builder.Attributes.Add("href", "https://twitter.com/share?text=" + title + " > ");
+            builder.Attributes.Add("data-url", url);
+            builder.Attributes.Add("rel", "canonical");
+            builder.Attributes.Add("title", "Compartir en Twitter");
+            imgBuilder.MergeAttribute("src", "/Content/themes/images/twitter.png");
+
+            imgBuilder.Attributes.Add("class", "shared-social");
+            builder.Attributes.Add("target=", "_blank");
+            builder.InnerHtml = imgBuilder.ToString(TagRenderMode.SelfClosing); ;
+
+            return new MvcHtmlString(builder.ToString());
+        }
+    }
+}
---
+++ PubliEventos.Web/ViewScripts/Shared/Social.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    (function () {
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol +
+        '//connect.facebook.net/en_US/all.js';
+        document.getElementById('fb-root').appendChild(e);
+    }());
+
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '321354234728519', status: true, cookie: true,
+            xfbml: true
+        });
+    };
+
+    window.fbShare = function(url, title, description, image) {
+        FB.ui(
+        {
+            method: 'feed',
+            name: title,
+            link: url,
+            picture: image,
+            caption: ' ',
+            description: description.length > 0 ? description.substring(0, 269) : " ",
+            message: ' '
+        });
+        return false;
+    };
+});
