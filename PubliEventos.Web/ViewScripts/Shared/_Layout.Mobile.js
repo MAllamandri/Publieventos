e5a0cc6f0bdf54df1efedc74314dc2d4d9009476
mallamandri <maxi_alias17@hotmail.com>
2015-05-27 19:55:06 -0300

    Adaptaciones mobile

---
+++ PubliEventos.Web/ViewScripts/Shared/_Layout.Mobile.js
@@ -0,0 +1,12 @@
+﻿//$(function () {
+//	$('#buttonMenu').hover(
+//		function () {
+//			$('#right').animate({ left: '-6%' }, 'slow', function () {
+//			});
+//		},
+//		function () {
+//			$('#right').animate({ left: '-120%' }, 'slow', function () {
+//			});
+//		}
+//	);
+//});
