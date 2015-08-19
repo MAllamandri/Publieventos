45e6eacde56753a9b74e37afc9a9d7cbd6298f95
mallamandri <maxi_alias17@hotmail.com>
2015-08-19 20:50:08 -0300

    Adaptaciones mobile.

--- PubliEventos.Web/ViewScripts/Shared/_Layout.Mobile.js
+++ PubliEventos.Web/ViewScripts/Shared/_Layout.Mobile.js
@@ -1,12 +1,18 @@
-﻿//$(function () {
-//	$('#buttonMenu').hover(
-//		function () {
-//			$('#right').animate({ left: '-6%' }, 'slow', function () {
-//			});
-//		},
-//		function () {
-//			$('#right').animate({ left: '-120%' }, 'slow', function () {
-//			});
-//		}
-//	);
-//});
+﻿$(function () {
+    $('.mobile-menu').click(function (event) {
+        $('nav').toggleClass('active');
+    });
+
+    //$(document).on("swipeleft swiperight", "#demo-page", function (e) {
+    //    // We check if there is no open panel on the page because otherwise
+    //    // a swipe to close the left panel would also open the right panel (and v.v.).
+    //    // We do this by checking the data that the framework stores on the page element (panel: open).
+    //    if ($.mobile.activePage.jqmData("panel") !== "open") {
+    //        if (e.type === "swipeleft") {
+    //            $("#right-panel").panel("open");
+    //        } else if (e.type === "swiperight") {
+    //            $("#left-panel").panel("open");
+    //        }
+    //    }
+    //});
+});
